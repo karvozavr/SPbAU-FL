f2073f20e3d02d24826646e4842d39477f1ab47e
Dmitriy Abramov <karvozavr@gmail.com>
2018-05-22 22:12:44 +0300

    Finished

--- hw5/test.sh
+++ hw5/test.sh
@@ -13,21 +13,3 @@ else
     echo -e "\n\nUnit tests failed.\n\n"
 fi
 
-for filename in $TESTS_VALID; do
-    output=$(python3 main.py --file $(realpath $filename) > /dev/null 2> /dev/null)
-    correct=$(cat "tests/valid/ans/$(basename $filename).ans")
-    if [ $? -eq 0 ]; then
-        echo "$filename OK"
-    else
-        echo "$filename FAILED"
-    fi
-done
-
-for filename in $TESTS_ERR; do
-    python3 main.py --file $(realpath $filename) > /dev/null 2> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "$filename OK"
-    else
-        echo "$filename FAILED"
-    fi
-done
